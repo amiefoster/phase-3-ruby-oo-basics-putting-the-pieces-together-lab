d2c93603af396aa6fb56b9afde5b06f4950a5ba5
Amie Foster <amie.n.foster@gmail.com>
2021-10-13 15:49:03 -0600

    completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize brand
+        @brand = brand
+    end
+
+    def cobble
+        if @condition == "new"
+           puts "Your shoe is as good as new!"
+        elsif @condition != "new"
+            @condition = "new"
+            puts "Your shoe is as good as new!"
+        end
+        
+    end
+end
+
+amie = Shoe.new("adidas")
+amie
+#binding.pry
